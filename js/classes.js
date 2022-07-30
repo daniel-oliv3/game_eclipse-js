24b85367c5d84747264334b3294cb8bae42856c7
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-30 09:23:24 -0300

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -9,12 +9,33 @@ class Sprite {
 
     draw(){
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2,false);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
         ctx.fillStyle = this.color;
         ctx.fill();
     }
 
 }
+
+class Sphere extends Sprite {
+    constructor(x, y, radius, color, angleUpdateValue, player){
+        super(x, y, radios, color);
+        this.angleUpdateValue = angleUpdateValue;
+        this.player = player;
+        this.angle = 0;
+    }
+
+    update(){
+        this.draw();
+        this.angle += this.angleUpdateValue;
+        if(Math.abs(this.angle) >= Math.PI*2){
+            this.angle = 0;
+        }
+
+        this.x = this.player.x + Math.cos(this.angle) * this.player.radius;
+        this.y = this.player.y + Math.sin(this.angle) * this.player.radius;
+    }
+}
+
 
 /* - */
 class Player extends Sprite {
