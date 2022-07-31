03e95e11dc78fcab3c02851cda1a13827a297d36
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-31 09:16:56 -0300

    Update f

--- js/script.js
+++ js/script.js
@@ -82,6 +82,15 @@ function checkProjectiles(){
         const p = projectiles[i];
         p.update();
         checkOffScreen(p, i);
+
+        for(let eIndex = enemies.length -1; eIndex >= 0; eIndex--){
+            const enemy = enemies[eIndex];
+            const distance = Math.hypot(p.x, - enemy.x, p.y - enemy.y);
+            if(distance < p.radius + enemy.radius){ //colisão
+                enemies.splice(eIndex, 1);
+                projectiles.splice(i, 1);
+            }
+        }
     }
 }
 
